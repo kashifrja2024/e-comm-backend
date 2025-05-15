fb62933601a04faca00a5273f53a9f2b1959f8cc
kashifrja2024 <kashifrja105@gmail.com>
2025-05-15 11:50:23 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 // MonoDB connection
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/e-commerce");
+mongoose.connect("mongodb+srv://kashifrja105:byJDlRSB0TKR1TLL@mern-appdb.kwggb8y.mongodb.net/?retryWrites=true&w=majority&appName=mern-appdb");
 
 
